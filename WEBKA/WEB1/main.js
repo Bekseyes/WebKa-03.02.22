f186e2c01aaeae0dca70dcd75e43afd175914593
Beksultan Kurman <96253140+Bekseyes@users.noreply.github.com>
2022-02-03 20:48:51 +0600

    Add files via upload

---
+++ WEBKA/WEB1/main.js
@@ -0,0 +1,10 @@
+function MyFunction() {
+    var first, second;
+    first = document.getElementById("firstname").value;
+    second = document.getElementById("groupname").value;
+
+    document.GetElementById("here").InnerHTML = first;
+    document.GetElementById("here").InnerHTML = second;
+
+
+}
